fd2df6314f22a7c2b1ac7830b91f625d1cc9d414
manofcult <70064845+manofcult@users.noreply.github.com>
2024-01-01 17:32:57 +0530

    Create kaboom.py

---
+++ kaboom.py
@@ -0,0 +1,59 @@
+from setuptools import setup
+import re
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('discord/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+readme = ''
+with open('README.rst') as f:
+    readme = f.read()
+
+extras_require = {
+    'voice': ['PyNaCl>=1.3.0,<1.6'],
+    'docs': [
+        'sphinx==4.4.0',
+        'sphinxcontrib_trio==1.1.2',
+        'sphinxcontrib-websupport',
+        'typing-extensions>=4.3,<5',
+        'sphinx-inline-tabs',
+    ],
+    'speed': [
+        'orjson>=3.5.4',
+        'aiodns>=1.1',
+        'Brotli',
+        'cchardet==2.1.7; python_version < "3.10"',
+    ],
+    'test': [
+        'coverage[toml]',
+        'pytest',
+        'pytest-asyncio',
+        'pytest-cov',
+        'pytest-mock',
+        'typing-extensions>=4.3,<5',
+        'tzdata; sys_platform == "win32"',
+    ],
+}
