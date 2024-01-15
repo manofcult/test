efc5a60e43ffaf25294688bbecb89f178e358918
manofcult <70064845+manofcult@users.noreply.github.com>
2024-01-15 21:17:27 +0530

    Create kolko.py

---
+++ kolko.py
@@ -0,0 +1,44 @@
+import io
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version():
+    regex = r"__version__\s=\s\'(?P<version>[\d\.]+?)\'"
+
+    path = ('aioelasticsearch', '__init__.py')
+
+    return re.search(regex, read(*path)).group('version')
+
+
+def read(*parts):
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
+
+    with io.open(filename, encoding='utf-8', mode='rt') as fp:
+        return fp.read()
+
+
+setup(
+    install_requires=[
+    ],
+    python_requires='>=3.5.3',
+    packages=['aioelasticsearch'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=['elasticsearch', 'asyncio', 'aiohttp'],
+)
