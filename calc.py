272256ec4eea13d0f740273ef15bb01ac62c9cb5
manofcult <70064845+manofcult@users.noreply.github.com>
2024-02-18 21:06:48 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,183 @@
+if 3/2 == 1:  # Because Python 2 does not know maths
+    input = raw_input  # Python 2 compatibility
+
+print('Welcome to this syntax!')
+print('It can help')
+num1 = int(input('Please choose your first number: '))
+sign = input('What do you want to do? +, -, /, or *: ')
+num2 = int(input('Please choose your second number: '))
+
+if num1 == 0 and sign == '+' and num2 == 0:
+    print("0+0 = 0")
+if num1 == 0 and sign == '+' and num2 == 1:
+    print("0+1 = 1")
+if num1 == 0 and sign == '+' and num2 == 2:
+    print("0+2 = 2")
+if num1 == 0 and sign == '+' and num2 == 3:
+    print("0+3 = 3")
+if num1 == 0 and sign == '+' and num2 == 4:
+    print("0+4 = 4")
+if num1 == 0 and sign == '+' and num2 == 5:
+    print("0+5 = 5")
+if num1 == 0 and sign == '+' and num2 == 6:
+    print("0+6 = 6")
+if num1 == 0 and sign == '+' and num2 == 7:
+    print("0+7 = 7")
+if num1 == 0 and sign == '+' and num2 == 8:
+    print("0+8 = 8")
+if num1 == 0 and sign == '+' and num2 == 9:
+    print("0+9 = 9")
+if num1 == 0 and sign == '+' and num2 == 10:
+    print("0+10 = 10")
+if num1 == 0 and sign == '+' and num2 == 11:
+    print("0+11 = 11")
+if num1 == 0 and sign == '+' and num2 == 12:
+    print("0+12 = 12")
+if num1 == 0 and sign == '+' and num2 == 13:
+    print("0+13 = 13")
+if num1 == 0 and sign == '+' and num2 == 14:
+    print("0+14 = 14")
+if num1 == 0 and sign == '+' and num2 == 15:
+    print("0+15 = 15")
+if num1 == 0 and sign == '+' and num2 == 16:
+    print("0+16 = 16")
+if num1 == 0 and sign == '+' and num2 == 17:
+    print("0+17 = 17")
+if num1 == 0 and sign == '+' and num2 == 18:
+    print("0+18 = 18")
+if num1 == 0 and sign == '+' and num2 == 19:
+    print("0+19 = 19")
+if num1 == 0 and sign == '+' and num2 == 20:
+    print("0+20 = 20")
+if num1 == 0 and sign == '+' and num2 == 21:
+    print("0+21 = 21")
+if num1 == 0 and sign == '+' and num2 == 22:
+    print("0+22 = 22")
+if num1 == 0 and sign == '+' and num2 == 23:
+    print("0+23 = 23")
+if num1 == 0 and sign == '+' and num2 == 24:
+    print("0+24 = 24")
+if num1 == 0 and sign == '+' and num2 == 25:
+    print("0+25 = 25")
+if num1 == 0 and sign == '+' and num2 == 26:
+    print("0+26 = 26")
+if num1 == 0 and sign == '+' and num2 == 27:
+    print("0+27 = 27")
+if num1 == 0 and sign == '+' and num2 == 28:
+    print("0+28 = 28")
+if num1 == 0 and sign == '+' and num2 == 29:
+    print("0+29 = 29")
+if num1 == 0 and sign == '+' and num2 == 30:
+    print("0+30 = 30")
+if num1 == 0 and sign == '+' and num2 == 31:
+    print("0+31 = 31")
+if num1 == 0 and sign == '+' and num2 == 32:
+    print("0+32 = 32")
+if num1 == 0 and sign == '+' and num2 == 33:
+    print("0+33 = 33")
+if num1 == 0 and sign == '+' and num2 == 34:
+    print("0+34 = 34")
+if num1 == 0 and sign == '+' and num2 == 35:
+    print("0+35 = 35")
+if num1 == 0 and sign == '+' and num2 == 36:
+    print("0+36 = 36")
+if num1 == 0 and sign == '+' and num2 == 37:
+    print("0+37 = 37")
+if num1 == 0 and sign == '+' and num2 == 38:
+    print("0+38 = 38")
+if num1 == 0 and sign == '+' and num2 == 39:
+    print("0+39 = 39")
+if num1 == 0 and sign == '+' and num2 == 40:
+    print("0+40 = 40")
+if num1 == 0 and sign == '+' and num2 == 41:
+    print("0+41 = 41")
+if num1 == 0 and sign == '+' and num2 == 42:
+    print("0+42 = 42")
+if num1 == 0 and sign == '+' and num2 == 43:
+    print("0+43 = 43")
+if num1 == 0 and sign == '+' and num2 == 44:
+    print("0+44 = 44")
+if num1 == 0 and sign == '+' and num2 == 45:
+    print("0+45 = 45")
+if num1 == 0 and sign == '+' and num2 == 46:
+    print("0+46 = 46")
+if num1 == 0 and sign == '+' and num2 == 47:
+    print("0+47 = 47")
+if num1 == 0 and sign == '+' and num2 == 48:
+    print("0+48 = 48")
+if num1 == 0 and sign == '+' and num2 == 49:
+    print("0+49 = 49")
+if num1 == 0 and sign == '+' and num2 == 50:
+    print("0+50 = 50")
+if num1 == 1 and sign == '+' and num2 == 0:
+    print("1+0 = 1")
+if num1 == 1 and sign == '+' and num2 == 1:
+    print("1+1 = 2")
+if num1 == 1 and sign == '+' and num2 == 2:
+    print("1+2 = 3")
+if num1 == 1 and sign == '+' and num2 == 3:
+    print("1+3 = 4")
+if num1 == 1 and sign == '+' and num2 == 4:
+    print("1+4 = 5")
+if num1 == 1 and sign == '+' and num2 == 5:
+    print("1+5 = 6")
+if num1 == 1 and sign == '+' and num2 == 6:
+    print("1+6 = 7")
+if num1 == 1 and sign == '+' and num2 == 7:
+    print("1+7 = 8")
+if num1 == 1 and sign == '+' and num2 == 8:
+    print("1+8 = 9")
+if num1 == 1 and sign == '+' and num2 == 9:
+    print("1+9 = 10")
+if num1 == 1 and sign == '+' and num2 == 10:
+    print("1+10 = 11")
+if num1 == 1 and sign == '+' and num2 == 11:
+    print("1+11 = 12")
+if num1 == 1 and sign == '+' and num2 == 12:
+    print("1+12 = 13")
+if num1 == 1 and sign == '+' and num2 == 13:
+    print("1+13 = 14")
+if num1 == 1 and sign == '+' and num2 == 14:
+    print("1+14 = 15")
+if num1 == 1 and sign == '+' and num2 == 15:
+    print("1+15 = 16")
+if num1 == 1 and sign == '+' and num2 == 16:
+    print("1+16 = 17")
+if num1 == 1 and sign == '+' and num2 == 17:
+    print("1+17 = 18")
+if num1 == 1 and sign == '+' and num2 == 18:
+    print("1+18 = 19")
+if num1 == 1 and sign == '+' and num2 == 19:
+    print("1+19 = 20")
+if num1 == 1 and sign == '+' and num2 == 20:
+    print("1+20 = 21")
+if num1 == 1 and sign == '+' and num2 == 21:
+    print("1+21 = 22")
+if num1 == 1 and sign == '+' and num2 == 22:
+    print("1+22 = 23")
+if num1 == 1 and sign == '+' and num2 == 23:
+    print("1+23 = 24")
+if num1 == 1 and sign == '+' and num2 == 24:
+    print("1+24 = 25")
+if num1 == 1 and sign == '+' and num2 == 25:
+    print("1+25 = 26")
+if num1 == 1 and sign == '+' and num2 == 26:
+    print("1+26 = 27")
+if num1 == 1 and sign == '+' and num2 == 27:
+    print("1+27 = 28")
+if num1 == 1 and sign == '+' and num2 == 28:
+    print("1+28 = 29")
+if num1 == 1 and sign == '+' and num2 == 29:
+    print("1+29 = 30")
+if num1 == 1 and sign == '+' and num2 == 30:
+    print("1+30 = 31")
+if num1 == 1 and sign == '+' and num2 == 31:
+    print("1+31 = 32")
+if num1 == 1 and sign == '+' and num2 == 32:
+    print("1+32 = 33")
+if num1 == 1 and sign == '+' and num2 == 33:
+    print("1+33 = 34")
+if num1 == 1 and sign == '+' and num2 == 34:
+    print("1+34 = 35")
+if num1 == 1 and sign == '+' and num2 == 35:
+    print("1+35 = 36")
